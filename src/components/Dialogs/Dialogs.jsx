e03e01f691cc65546c115c43ad656dda56cfb67f
Lena Furta <lenyce4ka@gmail.com>
2019-12-18 19:00:29 +0300

    23 lessons

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,10 +1,41 @@
 import React from 'react';
 import classes from './Dialogs.module.css';
+import {NavLink} from "react-router-dom";
 
 const Dialogs = (props) => {
     return (
-        <div className={classes.content}>
-            Dialogs
+        <div className={classes.dialogs}>
+            <div className={classes.dialogs_items}>
+                <div className={classes.dialog + ' ' + classes.active}>
+                    <NavLink to='/dialogs/1'>Kseniia</NavLink>
+                </div>
+                <div className={classes.dialog}>
+                    <NavLink to='/dialogs/2'>Nikita</NavLink>
+                </div>
+                <div className={classes.dialog}>
+                    <NavLink to='/dialogs/3'>Natalia</NavLink>
+                </div>
+                <div className={classes.dialog}>
+                    <NavLink to='/dialogs/4'>Tatiana</NavLink>
+                </div>
+                <div className={classes.dialog}>
+                    <NavLink to='/dialogs/5'>Anna</NavLink>
+                </div>
+                <div className={classes.dialog}>
+                    <NavLink to='/dialogs/6'>Maria</NavLink>
+                </div>
+            </div>
+            <div className={classes.messages}>
+                <div className={classes.message}>
+                    Hi
+                </div>
+                <div className={classes.message}>
+                    How are you?
+                </div>
+                <div className={classes.message}>
+                    I am fine
+                </div>
+            </div>
         </div>
     );
 };
